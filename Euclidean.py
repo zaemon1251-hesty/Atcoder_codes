6b825c2dcaec83be781936ddf54fbb0f46b56786
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-02-28 23:20:26 +0900

    add some algolisms

--- Euclidean.py
+++ Euclidean.py
@@ -11,6 +11,9 @@ def gcd2(m, n):
         m, n = n, m % n
     return m
 
+def mod(a,b):
+  return (a%b+b)%b
+
 # Extended Euclidean Algorithm
 def extgcd(a, b):
     if b:
@@ -23,7 +26,17 @@ def extgcd(a, b):
 def lcm(m, n):
     return m//gcd(m, n)*n
 
+#中国剰余定理（t ≡ b1 (mod m1), t ≡ b2 (mod m2)　となる最小のtを求める）
+def crt(b,m):
+    d, x, _ = extgcd(m[0],m[1])
+    if (b[1] - b[0])%d != 0:return (0, -1)
+    lm = lcm(m[0],m[1])
+    tmp = (b[1] - b[0]) // d * x % (m[1]//d)
+    r = (b[0] + m[0] * tmp) % m
+    return (r, lm)
+
 
 if __name__ =="__main__":
-    print(extgcd(10000,14))
-    #print(gcd(10,3))
+    print(extgcd(10000,14))#(2,-3,2143) -> 10000*-3 + 14*2143 = 2(=gcd(10000,14))
+    print(extgcd(4,-4))
+    
