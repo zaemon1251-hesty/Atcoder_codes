0545b2d82ac1b51aafe17d62a801eff8aee21f67
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-04-17 17:12:45 +0900

    add function to segtree

--- Euclidean.py
+++ Euclidean.py
@@ -39,4 +39,3 @@ def crt(b,m):
 if __name__ =="__main__":
     print(extgcd(10000,14))#(2,-3,2143) -> 10000*-3 + 14*2143 = 2(=gcd(10000,14))
     print(extgcd(4,-4))
-    
--- Segtree.py
+++ Segtree.py
@@ -16,6 +16,9 @@ class SegTree:
     init(init_val, ide_ele): 配列init_valで初期化 O(N)
     update(k, x): k番目の値をxに更新 O(logN)
     query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
+    find_rightest(a, b, x): [a,b)区間のx以下となる一番右のインデックスを取り出す O(logN)
+    find_leftest(a, b, x): [a,b)区間のx以下となる一番左のインデックスを取り出す O(logN)
+    get region(i): iを含み、区間積が tree[num + i - 1] となる区間 [l, r) を取り出す O(logN)
     """
     def __init__(self, init_val, segfunc, ide_ele):
         """
@@ -27,6 +30,7 @@ class SegTree:
         tree: セグメント木(1-index)
         """
         n = len(init_val)
+        self.n = n
         self.segfunc = segfunc
         self.ide_ele = ide_ele
         self.num = 1 << (n - 1).bit_length()
@@ -70,12 +74,76 @@ class SegTree:
             r >>= 1
         return res
 
+    def find_rightest(self, a, b, x):
+        """
+        [a,b)区間のx以下となる一番右のインデックスを取り出す
+        """
+        return self.find_rightest_sub(a, b, x, 0, 0, self.n)
 
-n,q=map(int,input().split())
-a=list(map(int,input().split()))
+    def find_leftest(self, a, b, x):
+        """
+        [a,r)区間のx以下となる一番左のインデックスを取り出す
+        """
+        return self.find_leftest_sub(a, b, x, 0, 0, self.n)
 
-tree=SegTree(a,segfunc,ide_ele)
-for i in range(q):
-    t,x,y=map(int,input().split())
-    if t==1:tree.update(x-1,y)
-    else:print(tree.query(x-1,y))
+    def find_rightest_sub(self, a, b, x, k, l, r):
+        """
+        find_ringtestのサブルーチン
+        [参考サイト]
+        https://algo-logic.info/segment-tree/
+        """
+        if self.tree[k] > x or r <= a or l>=b:
+            return max(a - 1, 0)
+        elif k >= self.num - 1:
+            return k - self.n - 1
+        else:
+            vr = self.find_rightest_sub(a, b, x, 2 * k + 2, (l + r) // 2, r)
+            if vr != a - 1:
+                return vr
+            else:
+                return self.find_rightest_sub(a, b, x, 2 * k + 1, l, (l + r) // 2)
+
+    def find_leftest_sub(self, a, b, x, k, l, r):
+        """
+        find_leftestのサブルーチン
+        [参考サイト]
+        https://algo-logic.info/segment-tree/
+        """
+        if self.tree[k] > x or r <= a or l>=b:
+            return min(b, self.n - 1)
+        elif k >= self.num - 1:
+            return k - self.n - 1
+        else:
+            vr = self.find_rightest_sub(a, b, x, 2 * k + 1, l, (r + l) // 2)
+            if vr != b:
+                return vr
+            else:
+                return self.find_rightest_sub(a, b, x, 2 * k + 2, (l + r) // 2, r)
+
+    def get_region(self, i):
+        idx = i + self.num - 1
+        return
+
+
+def routine_1():
+    n, q = map(int, input().split())
+    a = list(map(int, input().split()))
+    tree = SegTree(a, segfunc, ide_ele)
+    for _ in range(q):
+        t, x, y = map(int, input().split())
+        if t == 1:tree.update(x-1, y)
+        else:print(tree.query(x-1, y))
+
+
+def routine_2():
+    n = int(input())
+    a = list(map(int, input().split()))
+    tree = SegTree(a, segfunc, ide_ele)
+    for i in range(n):
+        r = tree.find_rightest(i + 1, n, a[i])
+        l = tree.find_leftest(0, i , a[i])
+        print("r:%s, l:%s, ans:%s"%(r, l, r - l + 1))
+
+
+if __name__ == "__main__":
+    routine_2()
