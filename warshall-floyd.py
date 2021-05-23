0cb24417acc2c37a8d6ee749ddc3302cfdf745ef
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-05-23 12:45:07 +0900

    abc202

--- warshall-floyd.py
+++ warshall-floyd.py
@@ -9,7 +9,7 @@ for i in range(n):
   x.append(a)
   y.append(b)
   z.append(c)
- 
+
 cost=[[inf]*n for _ in range(n)]
 keiro=[[[] for _ in range(n)] for _ in range(n)]
 #warshall_floyd
@@ -22,11 +22,11 @@ for k in range(n):
         if cost[i][j]>cost[i][k]+cost[k][j]:
           cost[i][j]=cost[i][k]+cost[k][j]
           keiro[i][j].append(k)
- 
+
 #bitDP
 # dp[s][i]は頂点集合sを全て通って頂点iで終わる道のうち最小となる辺の数
 dp=[[inf]*(n) for _ in range(2**n)]
- 
+
 #bitDPの部分,k!の問題を(2^k)(k^2)で解ける
 for s in range(1,2**n):
   for i in range(n):
@@ -42,8 +42,8 @@ for s in range(1,2**n):
         for t in keiro[0][i]:
           p|=1<<t
         dp[p][i]=min(dp[p][i],dp[p^(1<<i)][j] + cost[j][i])
-      
-        
+
+
 ans=inf
 for i in range(n):
   ans=min(ans,dp[-1][i]+cost[i][0])
