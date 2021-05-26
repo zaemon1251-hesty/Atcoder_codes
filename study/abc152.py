45ecd790b82ad895b12469225f6e4efc64ae8428
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-05-27 01:28:52 +0900

    shojin 2020/05/27

--- study/abc152.py
+++ study/abc152.py
@@ -1,3 +1,34 @@
+def get_nums(n):
+    return int(str(n)[0]), int(str(n)[-1])
+
+
+def count_strict_vals(n, a, b):
+    """
+    ABC152D
+    Nが二桁以上のときかつ（一桁のNが入力されたら0で返す）
+    A(最上位桁の数字a, 最下位桁の数字b)の桁数がNの桁数と同じであるとき、
+    制約を満たすAの総数を求める。
+    """
+    n =str(n)
+    a = str(a)
+    b = str(b)
+    if a > n[0] or len(n) == 1:
+        return 0
+    elif a < n[0]:
+        return pow(10, len(n) - 2)
+    else:
+        ans = 0
+        for i, v in zip(range(1, len(n) - 1), n[1:-1]):
+            if i == len(n) - 2 and b <= n[-1]:
+                ans += 1
+            ans += int(v) * pow(10, len(n) - 2 - i)
+        else:
+            ans += int(int(n[:-1]+b) <= int(n))
+
+        return ans
+
+
+
 def maina():
     n, m = map(int, input().split())
     print("Yes" if n == m else "No")
@@ -22,14 +53,24 @@ def mainc():
 
 
 def maind():
-    H = int(input())
-    ans = 1
-    from math import log2
-    ans = int(log2(H))
-    print(pow(2, ans + 1) - 1)
+    from itertools import permutations
+    N = int(input())
+
+    cnt = [[0]*10 for _ in range(10)]
+
+    for b in range(1, N + 1):
+        x, y = get_nums(b)
+        cnt[x][y] += 1
+
+    ans = 0
+    for i in range(1,10):
+        for j in range(1,10):
+            ans += cnt[i][j]*cnt[j][i]
+    print(ans)
+
+
 
 def maine():
-    from math import ceil
     h, n = map(int, input().split())
     A, B = [], []
     for i in range(n):
@@ -38,22 +79,10 @@ def maine():
         B.append(b)
 
 
-    dp = [[float("inf")] * (h + 1) for _ in range(n)]
-
-    dp[0][0] = 0
-    for j in range(1, h + 1):
-        dp[0][j] = ceil(j / A[0]) * B[0]
-
-    for i in range(1, n):
-        dp[i][0] = 0
-        for j in range(1, h + 1):
-            dp[i][j] = min(dp[i][j], dp[i - 1][j], ceil(j / A[i]) * B[i], dp[i][max(0, j - A[i])] + B[i]) #min(dp[i - 1][:max(0, j - A[i]) + 1])
-
-    print(dp[-1][-1])
 
 if __name__ == '__main__':
     #maina()
-    mainb()
+    #mainb()
     #mainc()
-    #maind()
+    maind()
     #maine()
