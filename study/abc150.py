a57c69fd4052cf3752c770e44ecb3f5184f8aaff
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-07-11 00:55:11 +0900

    shojin

---
+++ study/abc150.py
@@ -0,0 +1,29 @@
+def maind():
+    from math import gcd
+    from functools import reduce
+    N, M = map(int, input().split())
+    A = set(map(int, input().split()))
+    A = list(A)
+    A = sorted(A)
+    N = len(A)
+    r = []
+    for i in range(N):
+        A[i] //= 2
+
+    if N==1:
+        lcm = A[0]
+    else:
+        lcm = A[0]
+        for i in A[1:]:
+            lcm = lcm * i // gcd(lcm, i)
+
+    if any((lcm//A[i]) % 2 ==0 for i in range(N)):
+        print(0)
+        exit()
+
+    ans = M // lcm // 2
+    ans += M // lcm % 2
+
+    print(ans)
+
+maind()
---
+++ study/abc209.py
@@ -0,0 +1,91 @@
+def maina():
+    a,b = map(int, input().split())
+    print(b-a+1)
+
+
+def mainb():
+    N,x = map(int, input().split())
+    a = list(map(int, input().split()))
+    print("Yes" if sum(a) - N//2 <= x else "No")
+
+
+def mainc():
+    mod = 10**9 + 7
+    N = int(input())
+    C = list(map(int, input().split()))
+    C = sorted(C)
+    t = 0
+    ans = 1
+    for i in range(N):
+        ans *= max(0, C[i] - i)
+        ans %= mod
+    print(ans)
+
+def maind():
+    import sys
+    sys.setrecursionlimit(10**7)
+    N, Q = map(int, input().split())
+    G = [[] for _ in range(N)]
+
+    for _ in range(N-1):
+        a,b = map(int,input().split())
+        a,b = a-1,b-1
+        G[a].append(b)
+        G[b].append(a)
+
+    # Euler Tour Technique
+    S = []
+    FS = [0]*N
+    depth = [0]*N
+    def dfs(v, p, d):
+        depth[v] = d
+        FS[v] = len(S)
+        S.append(v)
+        for w in G[v]:
+            if w == p:
+                continue
+            dfs(w, v, d+1)
+            S.append(v)
+    dfs(0, -1, 0)
+
+    # Sparse Table
+    L = len(S)
+    lg = [0]*(L + 1)
+    for i in range(2, L+1):
+        lg[i] = lg[i >> 1] + 1
+    st = [None]*(lg[L] + 1)
+    st0 = st[0] = S
+    b = 1
+    for i in range(lg[L]):
+        st0 = st[i+1] = [p if depth[p] <= depth[q] else q for p, q in zip(st0, st0[b:])]
+        b <<= 1
+
+    # LCA O(1)
+    def query(u, v):
+        x = FS[u]; y = FS[v]
+        if x > y:
+            x, y = y, x
+        l = lg[y - x + 1]
+        px = st[l][x]; py = st[l][y - (1 << l) + 1]
+        return px if depth[px] <= depth[py] else py
+
+    q = [list(map(int,input().split())) for _ in range(Q)]
+
+    for x,y in q:
+        x,y = x-1,y-1
+        z = query(x,y)
+        if (depth[x] - depth[z]) % 2 == (depth[y] - depth[z]) % 2:
+            print("Town")
+        else:
+            print("Road")
+
+def maine():
+    pass
+
+if __name__ =="__main__":
+    mori = 20
+    #maina()
+    #mainb()
+    mainc()
+    #maind()
+    #maine()
