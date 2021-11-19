242f9b1606f51e2a3bf162e5575eedddff8efe1b
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-20 02:51:33 +0900

    shojin

--- study/abc154.py
+++ study/abc154.py
@@ -12,13 +12,16 @@ def maina():
 
     print(a, b)
 
+
 def mainb():
     print('x'*len(input()))
+
 
 def mainc():
     n = int(input())
     A = set(list(map(int, input().split())))
     print('YES' if len(A) == n else 'NO')
+
 
 def maind():
     N, K = map(int, input().split())
@@ -31,8 +34,32 @@ def maind():
 
     print(ans)
 
+
+def maine():
+    from functools import lru_cache
+    N = int(input())
+    K = int(input())
+
+    @lru_cache(maxsize=128, typed=False)
+    def digitDP(n, k):
+        if n < 10:
+            if k == 0:
+                return 1
+            elif k == 1:
+                return n
+            return 0
+        else:
+            ret = digitDP(n // 10, k)
+            if k >= 1:
+                ret += digitDP(n // 10, k-1) * (n % 10)
+                ret += digitDP(n // 10 - 1, k-1) * (9 - n % 10)
+            return ret
+    print(digitDP(N, K))
+
+
 if __name__ == '__main__':
-    #maina()
-    #mainb()
-    mainc()
-    #maind()
+    # maina()
+    # mainb()
+    # mainc()
+    # maind()
+    maine()
