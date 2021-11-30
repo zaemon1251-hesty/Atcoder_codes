3c0b10939628a097d85f84587c693568776b5f4b
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-30 10:36:48 +0900

    shojin

--- study/arc129.py
+++ study/arc129.py
@@ -10,4 +10,60 @@ def maina():
     print(ans)
 
 
-maina()
+def mainb():
+    from math import ceil
+    N = int(input())
+    inf = float('inf')
+    r = inf
+    l = -inf
+    ans = []
+    for i in range(N):
+        L, R = map(int, input().split())
+        r = min(r, R)
+        l = max(l, L)
+        if l <= r:
+            ans.append(0)
+        else:
+            ans.append(ceil((l - r) / 2))
+    print(*ans, sep='\n')
+
+
+def mainc():
+    from bisect import bisect
+
+    N = int(input())
+    A = [float('inf')]*N
+    A[-1] = 0
+    for i in range(N):
+        A[i] = A[i-1]+i+1
+
+    B = []
+    while N > 0:
+        idx = bisect(A, N)
+        N -= A[idx-1]
+        B.append(idx)
+    M = len(B)-1
+    if M == 0:
+        exit(print('7'*B[0]))
+
+    C = [1, 2, 3, 4, 5, 6, 8, 9]
+
+    def dfs(idx=1, pre=[]):
+        if len(pre) == 2*M+1:
+            exit(print(''.join(pre)))
+
+        for c in C:
+            fit = True
+            for i in range(len(pre)):
+                a = ''.join(pre[i:])+str(c)
+                if int(a) % 7 == 0:
+                    fit = False
+                    break
+
+            if fit:
+                dfs(idx+1, pre+[str(c), '7'*B[idx]])
+
+    dfs(1, ['7'*B[0]])
+
+
+mainc()
