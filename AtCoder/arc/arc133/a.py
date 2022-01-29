0f8859d0286278f837c832e277b965426d9c95a8
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-01-30 01:15:21 +0900

    shojin

---
+++ AtCoder/arc/arc133/a.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/pypy3
+import sys
+readline = sys.stdin.buffer.readline
+sys.setrecursionlimit(10**6)
+
+n = int(readline())
+a = list(map(int, readline().split()))
+
+x = a[-1]
+for i in range(n-1):
+    if a[i] > a[i+1]:
+        x = a[i]
+        break
+
+a = [v for v in a if v != x]
+print(*a)
