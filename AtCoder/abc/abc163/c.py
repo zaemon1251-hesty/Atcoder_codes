586dde669aea27a696dfba4b621ae7ec4b1be743
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-02-01 00:55:09 +0900

    shojin

---
+++ AtCoder/abc/abc163/c.py
@@ -0,0 +1,6 @@
+N = int(input())
+A = list(map(lambda x: int(x) - 1, input().split()))
+b = [0 for _ in range(N)]
+for i in range(N-1):
+    b[A[i]] += 1
+print(*b, sep='\n')
---
+++ AtCoder/abc/abc163/d.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+mod = 10**9 + 7
+ans = 0
+cr = sum(range(K))
+ut = sum(range(N - K + 1, N + 1))
+for i in range(K, N+2):
+    ans += ut - cr + 1
+    ans %= mod
+    cr += i
+    ut += N - i
+print(ans % mod)
