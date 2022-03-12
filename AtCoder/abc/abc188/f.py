797de2ec46c55fc6c0e29596437f2e957597b1a8
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-03-12 16:43:26 +0900

    shojin 2

--- AtCoder/abc/abc188/f.py
+++ AtCoder/abc/abc188/f.py
@@ -1,7 +1,21 @@
+import sys
+from functools import lru_cache
+sys.setrecursionlimit(10**6)
+
+
 def main():
-    N = int(input())
-    H, W = map(int, input().split())
-    A = list(map(int, input().split()))
+    X, Y = map(int, input().split())
+
+    @lru_cache(None)
+    def solve(y):
+        if y <= 1:
+            return abs(X - y)
+        if y % 2 == 0:
+            return min(solve(y // 2) + 1, abs(X - y))
+        else:
+            return min(solve((y + 1) // 2) + 2,
+                       solve((y - 1) // 2) + 2, abs(X - y))
+    print(solve(Y))
 
 
 if __name__ == '__main__':
