6f5d77c3daa47069f2711e86222873eae04faacc
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-04-07 02:36:29 +0900

    shojin abc 192 F

--- AtCoder/abc/abc192/f.py
+++ AtCoder/abc/abc192/f.py
@@ -1,27 +1,23 @@
 def main():
-    inf = 1 << 60
     N, X = map(int, input().split())
     A = list(map(int, input().split()))
 
-    ans = inf
-    for k in range(1, N + 1):
-        # dp[i][use][r]] = i番目まででuseコ使って初期合計魔力が r[mod k] のときの残り魔力の最小値
-        dp = [[
-            [inf] * 101
-            for _ in range(101)]
-            for _ in range(101)]
-        dp[0][0][X % k] = X
-        for i in range(N):
-            for use in range(k + 1):
-                for r in range(k):
-                    if dp[i + 1][use][r] > dp[i][use][r]:
-                        dp[i + 1][use][r] = dp[i][use][r]
-                    next_resd = (((r - A[i]) % k) + k) % k
-                    if use < k and \
-                            dp[i + 1][use + 1][next_resd] > dp[i][use][r] - A[i]:
-                        dp[i + 1][use + 1][next_resd] = dp[i][use][r] - A[i]
-        if dp[N][k][0] != inf:
-            ans = min(ans, dp[N][k][0] // k)
+    ans = X - max(A)
+    for i in range(2, N + 1):
+        # dp[j][r] = 魔法力の合計をiで割ったあまりがrになる、j個の魔法力の合計の最大値
+        dp = [[-1] * i for _ in range(i + 1)]
+        # 魔法力の合計をiで割ったあまりが0になる、0個の魔法力の合計の最大値は0である。(初期値)
+        dp[0][0] = 0
+        for a in A:
+            for j in range(i - 1, -1, -1):
+                for k in range(i):
+                    if dp[j][k] != -1:
+                        dp[j + 1][(k + a) % i] = \
+                            max(dp[j + 1][(k + a) % i], dp[j][k] + a)
+        need = X % i
+        #print(i, dp[i][need])
+        if dp[i][need] != -1:
+            ans = min(ans, (X - dp[i][need]) // i)
     print(ans)
 
 
