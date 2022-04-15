692cf844b8145e15b64565dc9704045d6677d436
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-04-16 00:27:09 +0900

    shojin

---
+++ AtCoder/abc/abc194/a.py
@@ -0,0 +1,15 @@
+def main():
+    A, B = map(int, input().split())
+    A += B
+    ans = 4
+    if A >= 15 and B >= 8:
+        ans = 1
+    elif A >= 10 and B >= 3:
+        ans = 2
+    elif A >= 3:
+        ans = 3
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
