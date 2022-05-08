a6783585773f62de80b2c787291555aece92992a
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-05-08 23:49:17 +0900

    shojin abc 250

---
+++ AtCoder/abc/abc250/a.py
@@ -0,0 +1,12 @@
+def main():
+    H, W = map(int, input().split())
+    R, C = map(int, input().split())
+    ans = 0
+    for nx in range(1, H + 1):
+        for ny in range(1, W + 1):
+            ans += (abs(R - nx) + abs(C - ny) == 1)
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
