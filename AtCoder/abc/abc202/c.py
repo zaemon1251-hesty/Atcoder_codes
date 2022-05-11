dc95fd5b26b3491ad3cdad80145bafbceb67852e
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-05-11 22:22:34 +0900

    shojin

--- AtCoder/abc/abc202/c.py
+++ AtCoder/abc/abc202/c.py
@@ -1,11 +1,17 @@
-n = int(input())
 from collections import Counter
-from bisect import bisect_left, bisect_right
-A = sorted(list(map(int, input().split())))
-B = list(map(int, input().split()))
-C = Counter(list(map(lambda x:int(x) - 1, input().split())))
-ans = 0
-for k,v in C.items():
-    val = B[k]
-    ans += v * (bisect_right(A, val) - bisect_left(A, val))
-print(ans)
+
+
+def main():
+    n = int(input())
+    A = Counter(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = Counter(map(lambda x: int(x) - 1, input().split()))
+    ans = 0
+    for k, v in C.items():
+        val = B[k]
+        ans += v * A[val]
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
--- AtCoder/abc/abc202/d.py
+++ AtCoder/abc/abc202/d.py
@@ -1,22 +1,29 @@
-A, B, k = map(int, input().split())
-S = A + B
-ans = ""
-for i in range(S):
-    if k == 1:
-        for _ in range(A):
-            ans += "a"
-        for _ in range(B):
-            ans += "b"
-        print(ans)
-        exit()
-    s = cmb(A - 1 + B, A -1)
-    if k <= s:
-        ans += "a"
-        A -= 1
+def cmb(n, r):
+    r = min(n - r, r)
+    a = 1
+    b = 1
+    for k in range(r):
+        a *= (n - r + 1 + k)
+
+        b *= (k + 1)
+
+    return a // b
+
+
+def gen(A_: int, B_: int, k_: int) -> str:
+    if k_ <= 1 or A_ < 1 or B_ < 1:
+        return "a" * max(A_, 0) + "b" * max(B_, 0)
+    s = cmb(A_ - 1 + B_, A_ - 1)
+    if k_ <= s:
+        return "a" + gen(A_ - 1, B_, k_)
     else:
-        ans += "b"
-        B -= 1
-        k -= s
-print(ans)
-bal変数の都合
-0
+        return "b" + gen(A_, B_ - 1, k_ - s)
+
+
+def main():
+    A, B, k = map(int, input().split())
+    print(gen(A, B, k))
+
+
+if __name__ == "__main__":
+    main()
