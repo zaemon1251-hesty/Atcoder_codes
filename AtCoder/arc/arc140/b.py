316a82856933fa08fb68789529111f5f94b66281
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-05-19 16:12:35 +0900

    shojin

---
+++ AtCoder/arc/arc140/b.py
@@ -0,0 +1,21 @@
+def main():
+    n = int(input())
+    s = input()
+    xsum, m = 0, 0
+
+    for i in range(1, n - 1):
+        if s[i - 1:i + 2] == "ARC":
+            l, r = i - 1, i + 1
+            while l - 1 >= 0 and s[l - 1] == "A":
+                l -= 1
+            while r + 1 < n and s[r + 1] == "C":
+                r += 1
+            x = min(i - l, r - i)
+            xsum += x
+            m += 1
+
+    print(min(xsum, m * 2))
+
+
+if __name__ == '__main__':
+    main()
