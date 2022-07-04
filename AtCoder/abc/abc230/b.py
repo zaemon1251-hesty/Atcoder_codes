d9c37631289f8797167115482a27b1f1f96c4758
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-07-04 09:49:47 +0900

    shojin abc 230

--- AtCoder/abc/abc230/b.py
+++ AtCoder/abc/abc230/b.py
@@ -1,11 +1,11 @@
-N, D = map(int, input().split())
-S = [list(map(int, input().split())) for _ in range(N)]
-S.sort(key=lambda x: (x[1]))
-removed = -1
-ans = 0
-for a, b in S:
-    # a が removed より大きい = まだ取り除いてない
-    if a > removed:
-        removed = b + D - 1
-        ans += 1
-print(ans)
+def main():
+    S = input()
+    T = "oxx" * 10**3
+    if S in T:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == '__main__':
+    main()
