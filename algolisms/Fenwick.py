07e877ac83ba8b1ea7abbd2c58a87abac94f6dcb
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-09-09 12:22:21 +0900

    shojin abc 134

--- algolisms/Fenwick.py
+++ algolisms/Fenwick.py
@@ -1,47 +1,52 @@
-
-class BIT:
-    """
-    Binary Indexed Tree
-    演算は足し算
-    """
-
-    def __init__(self, n, func, ele) -> None:
+class Fenwick:
+    def __init__(self, n):
         self.n = n
-        self.ele = ele
-        self.func = func
-        self.bit = [ele]*(n+1)
+        self.n0 = 2**(n - 1).bit_length()
+        self.data = [0] * (n + 1)
+        self.el = [0] * (n + 1)
 
-    def add(self, i, x) -> None:
-        i += 1
-        while i <= self.n:
-            self.bit[i] = self.func(self.bit[i], x)
-            i += i & -i
-
-    def get(self, l, r) -> int:
-        # [l+1,r]の区間で演算される
-        res = self.ele
-        while l:
-            res = self.func(res, self.bit[l])
-            l -= l & -l
-        while r:
-            res = self.func(res, self.bit[r])
-            r -= r & -r
-        return res
+    def init(self, A):
+        self.data[1:] = A
+        for i in range(1, self.n):
+            if i + (i & -i) <= self.n:
+                self.data[i + (i & -i)] += self.data[i]
 
     def sum(self, i):
-        # [0, i] での演算
-        i += 1
-        res = self.ele
+        s = 0
         while i > 0:
-            res = self.func(res, self.bit[i])
+            s += self.data[i]
             i -= i & -i
-        return res
+        return s
+
+    def add(self, i, x):
+        # assert i > 0
+        self.el[i] += x
+        while i <= self.n:
+            self.data[i] += x
+            i += i & -i
+
+    def get(self, i, j=None):
+        if j is None:
+            return self.el[i]
+        return self.sum(j) - self.sum(i)
+
+    def lower_bound(self, x):
+        """returns i = &t = min({t | t > x})"""
+        w = i = 0
+        k = self.n0
+        while k:
+            if i + k <= self.n and w + self.data[i + k] <= x:
+                w += self.data[i + k]
+                i += k
+            k >>= 1
+        # assert self.get(0, i) <= x < self.get(0, i+1)
+        return i + 1
 
 
 # これはXor演算
 n, q = map(int, input().split())
 a = list(map(int, input().split()))
-bit = [0]*(n+1)
+bit = [0] * (n + 1)
 
 
 def add(i, x):
@@ -69,6 +74,6 @@ for i in range(n):
 for _ in range(q):
     t, x, y = map(int, input().split())
     if t == 1:
-        add(x-1, y)
+        add(x - 1, y)
     else:
-        print(get(x-1, y))
+        print(get(x - 1, y))
