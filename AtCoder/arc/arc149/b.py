28ab015c7217175f0cd19b8355f0625eef2d0af9
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-10-04 22:23:34 +0900

    shojin arc 149

---
+++ AtCoder/arc/arc149/b.py
@@ -0,0 +1,38 @@
+from bisect import bisect_left
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def main():
+    def li():
+        return list(map(int, input().split()))
+
+    def mi():
+        return map(int, input().split())
+
+    def ii():
+        return int(input())
+
+    N = ii()
+    A = li()
+    B = li()
+
+    SORTED_A = sorted(enumerate(A), key=lambda x: (x[1], x[0]))
+    iset = [ai[0] for ai in SORTED_A]
+    B_ = [B[i] for i in iset]
+
+    INF = 1 << 60
+    LIS = [INF] * (N + 1)
+    for b in B_:
+        i = bisect_left(LIS, b)
+        LIS[i] = b
+
+    ANS_B = bisect_left(LIS, INF)
+    print(N + ANS_B)
+
+
+if __name__ == '__main__':
+    main()
---
+++ AtCoder/arc/arc149/c.py
@@ -0,0 +1,84 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+ExceptionalEdges = {
+    3: {
+        'odd': [1, 3, 7],
+        'even': [8, 6, 2]
+    },
+    4: {
+        'odd': [1, 3, 5, 7],
+        'even': [8, 6, 4, 2]
+    },
+    5: {
+        'odd': [21, 1, 3, 9, 15],
+        'even': [24, 8, 6, 12, 18]
+    }
+}
+
+
+def main():
+    def li():
+        return list(map(int, input().split()))
+
+    def mi():
+        return map(int, input().split())
+
+    def ii():
+        return int(input())
+
+    N = ii()
+    A = [-1] * (N**2)
+    deck = set([i for i in range(1, N**2 + 1)])
+
+    even_i = (N**2 + 1) // 2
+    odd_i = even_i - 1
+
+    # fill edge numbers
+    if N <= 5:
+        for od, ev in zip(
+                ExceptionalEdges[N]['odd'][::-1],
+                ExceptionalEdges[N]['even']):
+            deck.remove(od)
+            deck.remove(ev)
+
+            A[odd_i] = od
+            A[even_i] = ev
+
+            odd_i -= 1
+            even_i += 1
+    else:
+        d = 3
+        cnt = 0
+        while cnt < 2 * N:
+            deck.remove(d)
+
+            if d % 2 == 1:
+                A[odd_i] = d
+                odd_i -= 1
+            else:
+                A[even_i] = d
+                even_i += 1
+            cnt += 1
+            d += 3
+
+    # fill the rest numbers
+    for d in deck:
+        if d % 2 == 1:
+            A[odd_i] = d
+            odd_i -= 1
+        else:
+            A[even_i] = d
+            even_i += 1
+
+    # OUTPUT
+    for st in range(0, N**2, N):
+        print(*A[st:st + N])
+
+
+if __name__ == '__main__':
+    main()
