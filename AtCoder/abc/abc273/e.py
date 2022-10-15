52e397030e6c170fbd46e6aff56651d469d7309d
zaemon1251-hesty <zaemon1251@gmail.com>
2022-10-16 00:21:39 +0900

    shojin abc 273

---
+++ AtCoder/abc/abc273/e.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+Q = int(input())
+A = [-1]
+prev = [-1]
+note = dict()
+idx = 0
+n = 0
+for i in range(Q):
+    x = list(input().split())
+    if x[0] == "ADD":
+        a = int(x[1])
+        A.append(a)
+        prev.append(idx)
+        n += 1
+        idx = n
+    elif x[0] == "DELETE":
+        if idx != 0:
+            idx = prev[idx]
+    elif x[0] == "SAVE":
+        a = int(x[1])
+        note[a] = idx
+    elif x[0] == "LOAD":
+        a = int(x[1])
+        if a not in note.keys():
+            idx = 0
+        else:
+            idx = note[a]
+    print(A[idx], end=" ")
